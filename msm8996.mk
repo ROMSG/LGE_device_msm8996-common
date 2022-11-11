8b2640896027e7065370886574caf92b01424618
xxseva44 <dev.xxseva44@gmail.com>
2022-11-11 17:57:46 -0500

    TEMP: Remove radio for now

--- msm8996.mk
+++ msm8996.mk
@@ -356,16 +356,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
-# RIL
-PRODUCT_PACKAGES += \
-    android.hardware.radio@1.4 \
-    android.hardware.radio@1.4.vendor \
-    android.hardware.radio.config@1.1 \
-    android.hardware.radio.config@1.1.vendor \
-    android.hardware.radio.deprecated@1.0 \
-    android.hardware.radio.deprecated@1.0.vendor \
-    libprotobuf-cpp-full \
-    librmnetctl 
+# RIL Not functional at the moment.
+#PRODUCT_PACKAGES += \
+#    android.hardware.radio@1.4 \
+#    android.hardware.radio@1.4.vendor \
+#    android.hardware.radio.config@1.1 \
+#    android.hardware.radio.config@1.1.vendor \
+#    android.hardware.radio.deprecated@1.0 \
+#    android.hardware.radio.deprecated@1.0.vendor \
+#    libprotobuf-cpp-full \
+#    librmnetctl 
     
 # Seccomp policy
 PRODUCT_COPY_FILES += \
